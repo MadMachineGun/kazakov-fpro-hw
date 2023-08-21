ae172602569ca018a34e3738adc539199842643b
Yurii Kazakov <gfnvortex@gmail.com>
2023-08-21 10:44:35 +0300

    hw_3.3_final

--- homework_3.3.js
+++ homework_3.3.js
@@ -1,11 +1,53 @@
-let age = prompt("How old are you? ", "Your answer: ");
-age = Number(age);
-if (typeof (age) == "number" && String(age) != NaN) {
-    if (age > 30) {
-        console.log("Go rest...");
-    } else {
-        console.log("Work!");
-    }
+
+let birthYear = prompt("Enter your birth year:");
+let city = prompt("Enter the city you live in:");
+let favoriteSport = prompt("Enter your favorite sport:");
+
+if (birthYear === null || city === null || favoriteSport === null) {
+    alert("It's a pity that you didn't want to provide your birth year, city, or favorite sport.");
 } else {
-    console.log("Please input number!")
+    let currentYear = new Date().getFullYear();
+    let age = currentYear - parseInt(birthYear);
+
+    let message = `Your age: ${age}\n`;
+
+    if (city === "Kyiv" || city === "Washington" || city === "London") {
+        let country = "";
+        if (city === "Kyiv") country = "Ukraine";
+        else if (city === "Washington") country = "USA";
+        else if (city === "London") country = "UK";
+        message += `You live in the capital city of ${country}.\n`;
+    } else {
+        message += `You live in the city of ${city}.\n`;
+    }
+
+    let champions = {
+        "football": "Lionel Messi",
+        "basketball": "LeBron James",
+        "tennis": "Stewart Errol Ungar"
+    };
+
+    if (favoriteSport in champions) {
+        message += `Cool! Do you want to become the next ${champions[favoriteSport]}?\n`;
+    } else {
+        message += `Your favorite sport is ${favoriteSport}.\n`;
+    }
+
+    alert(message);
 }
+
+
+
+// LESSON_3_teacher's_code:
+
+// let age = prompt("How old are you? ", "Your answer: ");
+// age = Number(age);
+// if (typeof (age) == "number" && String(age) != NaN) {
+//     if (age > 30) {
+//         console.log("Go rest...");
+//     } else {
+//         console.log("Work!");
+//     }
+// } else {
+//     console.log("Please input number!")
+// }
