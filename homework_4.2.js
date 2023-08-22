8fb6ca402b6ced66b9f217a79afbea878715f54d
Yurii Kazakov <gfnvortex@gmail.com>
2023-08-22 11:49:16 +0300

    hw_4.2

--- homework_4.2.js
+++ homework_4.2.js
@@ -1,14 +1,29 @@
-//Task_1:
-
+//Task_1: Вивести на сторінку в один рядок через кому числа від 10 до 20.
 //Solution_of_the_task_1:
 
-//Task_2:
+let numbers = [];
+for (let i = 10; i <= 20; i++) {
+    numbers.push(i);
+}
+console.log(numbers.join(", "));
 
+//Task_2: Вивести квадрати чисел від 10 до 20.
 //Solution_of_the_task_2:
 
-//Task_3:
+let squares = [];
+for (let i = 10; i <= 20; i++) {
+    squares.push(i * i);
+}
+console.log(squares.join(", "));
 
+//Task_3:Вивести таблицю множення на 7.
 //Solution_of_the_task_3:
+
+let multForSeven = [];
+for (let i = 1; i <= 10; i++) {
+    multForSeven.push(i * 7);
+}
+console.log(multForSeven.join(", "));
 
 //Task_4:
 
