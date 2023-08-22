7e4dd82acc01b6983597fa8d83bf6e1593ee9f0e
Yurii Kazakov <gfnvortex@gmail.com>
2023-08-22 16:38:15 +0300

    hw_4.2

--- homework_4.2.js
+++ homework_4.2.js
@@ -1,43 +1,49 @@
-//Task_1: Вивести на сторінку в один рядок через кому числа від 10 до 20.
-//Solution_of_the_task_1:
+// Task_1: Вивести на сторінку в один рядок через кому числа від 10 до 20.
+// let numbers = [];
+// for (let i = 10; i <= 20; i++) {
+//     numbers.push(i);
+// }
+// console.log('Task 1:\n',numbers.join(", "));
+console.log('Task 1:\n',Array.from({ length: 11 }, (_, i) => i + 10).join(', '));
 
-let numbers = [];
-for (let i = 10; i <= 20; i++) {
-    numbers.push(i);
+// Task_2: Вивести квадрати чисел від 10 до 20.
+// let squares = [];
+// for (let i = 10; i <= 20; i++) {
+//     squares.push(i * i);
+// }
+// console.log('Task 2:\n',squares.join(", "));
+console.log('Task 2:\n',Array.from({ length: 11 }, (_, i) => (i + 10) ** 2).join(', '));
+
+// Task_3:Вивести таблицю множення на 7.
+console.log('Task 3:');
+for (let i = 1; i < 10; i++) {
+    console.log(`7 * ${i} = ${7 * i}`);
 }
-console.log(numbers.join(", "));
 
-//Task_2: Вивести квадрати чисел від 10 до 20.
-//Solution_of_the_task_2:
-
-let squares = [];
-for (let i = 10; i <= 20; i++) {
-    squares.push(i * i);
+//Task_4: Знайти суму всіх цілих чисел від 1 до 15.
+console.log('Task 4:');
+let sum = 0;
+for (let i = 1; i <= 15; i++) {
+    sum += i;
+    console.log(`${i} + ${sum - i} = ${sum}`);
 }
-console.log(squares.join(", "));
 
-//Task_3:Вивести таблицю множення на 7.
-//Solution_of_the_task_3:
+//Task_5: Знайти добуток усіх цілих чисел від 15 до 35.
+// let div = 1;
+// for (let i = 15; i <= 35; i++) div *= i;
+// console.log('Task 5:\n','Result:', div);
 
-let multForSeven = [];
-for (let i = 1; i <= 10; i++) {
-    multForSeven.push(i * 7);
+console.log('Task 5:');
+let mult = 1;
+for (let i = 15; i <= 35; i++) {
+    mult *= i;
+    console.log(`${i} * ${mult / i} = ${mult}`);
 }
-console.log(multForSeven.join(", "));
 
-//Task_4:
+//Task_6:
 
-//Solution_of_the_task_4:
-
-//Task_5:
-
-//Solution_of_the_task_5:
 
 //Task_7:
-
-//Solution_of_the_task_7:
-
-//Task_8:
 
 //Solution_of_the_task_8:
 
