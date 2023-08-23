cd5e65e9f7629620fc80ffc153a1bc27711524e4
Yurii Kazakov <gfnvortex@gmail.com>
2023-08-23 12:10:41 +0300

    hw_4.2

--- homework_4.2.js
+++ homework_4.2.js
@@ -4,7 +4,7 @@
 //     numbers.push(i);
 // }
 // console.log('Task 1:\n',numbers.join(", "));
-console.log('Task 1:\n',Array.from({ length: 11 }, (_, i) => i + 10).join(', '));
+console.log('Task 1:\n', Array.from({length: 11}, (_, i) => i + 10).join(', '));
 
 // Task_2: Вивести квадрати чисел від 10 до 20.
 // let squares = [];
@@ -12,7 +12,7 @@ console.log('Task 1:\n',Array.from({ length: 11 }, (_, i) => i + 10).join(', '))
 //     squares.push(i * i);
 // }
 // console.log('Task 2:\n',squares.join(", "));
-console.log('Task 2:\n',Array.from({ length: 11 }, (_, i) => (i + 10) ** 2).join(', '));
+console.log('Task 2:\n', Array.from({length: 11}, (_, i) => (i + 10) ** 2).join(', '));
 
 // Task_3:Вивести таблицю множення на 7.
 console.log('Task 3:');
@@ -21,39 +21,72 @@ for (let i = 1; i < 10; i++) {
 }
 
 //Task_4: Знайти суму всіх цілих чисел від 1 до 15.
-console.log('Task 4:');
 let sum = 0;
 for (let i = 1; i <= 15; i++) {
     sum += i;
-    console.log(`${i} + ${sum - i} = ${sum}`);
 }
+console.log('Task 4:\n', sum);
 
 //Task_5: Знайти добуток усіх цілих чисел від 15 до 35.
-// let div = 1;
-// for (let i = 15; i <= 35; i++) div *= i;
-// console.log('Task 5:\n','Result:', div);
 
-console.log('Task 5:');
-let mult = 1;
+let seq = 1;
 for (let i = 15; i <= 35; i++) {
-    mult *= i;
-    console.log(`${i} * ${mult / i} = ${mult}`);
+    seq *= i;
+}
+console.log('Task 5:\n', seq);
+
+//Task_6: Знайти середнє арифметичне всіх цілих чисел від 1 до 500.
+let totalSum = 0;
+for (let i = 1; i <= 500; i++) {
+    totalSum += i;
+}
+console.log("Task 6:\n", totalSum / 500);
+
+//Task_7: Вивести всі числа в діапазоні від 100 до 200 кратні 3.
+console.log("Task 7:")
+for (let i = 100; i <= 200; i++) {
+    if (i % 3 === 0) {
+        console.log(i);
+    }
 }
 
-//Task_6:
+//Task_8: Дано натуральне число. Знайти та вивести на сторінку всі його дільники.
+console.log("Task 8:")
+let a = prompt("Enter a natural number:");
+a = +a;
+if (!isNaN(a) && a > 0) {
+    console.log(`Divisors of ${a}:`);
+    for (let i = 1; i <= a; i++) {
+        if (a % i === 0) {
+            console.log(i);
+        }
+    }
+} else {
+    console.log("Invalid input!");
+}
 
+//Task_9: Визначити кількість його парних дільників.
+console.log("Task 9:");
 
-//Task_7:
+let num = prompt("Enter a natural number:");
+num = +num;
+let evenDivisorsCount = 0;
 
-//Solution_of_the_task_8:
+if (!isNaN(num) && num > 0) {
+    console.log(`Divisors of ${num}:`);
+    for (let i = 1; i <= num; i++) {
+        if (num % i === 0) {
+            console.log(i);
+            if (i % 2 === 0) {
+                evenDivisorsCount++;
+            }
+        }
+    }
+    console.log(`Number of even divisors: ${evenDivisorsCount}`);
+} else {
+    console.log("Invalid input!");
+}
 
-//Task_9:
-
-//Solution_of_the_task_9:
-
-//Task_10:
-
-//Solution_of_the_task_10:
 
 //Task_11:
 
