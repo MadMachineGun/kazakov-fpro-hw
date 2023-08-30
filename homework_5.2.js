110bc0cf43ba805df6f87c3e35b717831d491398
Yurii Kazakov <gfnvortex@gmail.com>
2023-08-30 10:19:07 +0300

    homework_5.2_+method_forEach_solution

--- homework_5.2.js
+++ homework_5.2.js
@@ -263,7 +263,7 @@ console.log("Modified array:", modifiedArray);
 // console.log("Task 10:\nЗнайти найбільший серед елементів масиву, остальні обнулити.");
 // console.log("Max value:", maxEl);
 // console.log("Modified array:", maxElement);
-
+//
 // const maxElement = [16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54, 76, -4, 12, -35, 4, 47];
 // let maxInd = maxElement.reduce((maxIndex, currentValue, currentIndex, array) => currentValue > array[maxIndex] ? currentIndex : maxIndex, 0);
 //
@@ -276,3 +276,158 @@ console.log("Modified array:", modifiedArray);
 // console.log("Task 10:\nЗнайти найбільший серед елементів масиву, остальні обнулити.");
 // console.log("Max value:", maxElement[maxInd]);
 // console.log("Modified array:", maxElement);
+
+
+// // Use_forEach_method
+// // Task_1:
+// const array = [16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54, 76, -4, 12, -35, 4, 47];
+// let sum = 0;
+// let positiveCount = 0;
+//
+// array.forEach((element) => {
+//     if (element > 0) {
+//         sum += element;
+//         positiveCount++;
+//     }
+// });
+//
+// console.log("Task 1:\nДан масив [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47]\nЗнайти суму та кількість позитивних елементів.");
+// console.log("Sum of positive elements:", sum);
+// console.log("Number of positive elements:", positiveCount);
+//
+// // Task_2:
+// const minArr = [16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54, 76, -4, 12, -35, 4, 47];
+// let minValue = minArr[0];
+// let minIndex = 0;
+//
+// minArr.forEach((element, index) => {
+//     if (element < minValue) {
+//         minValue = element;
+//         minIndex = index;
+//     }
+// });
+//
+// console.log("Task 2:\nЗнайти мінімальний елемент масиву та його порядковий номер.");
+// console.log("User array:", minArr);
+// console.log("Min value:", minValue);
+// console.log("Index of min value:", minIndex);
+//
+// // Task_3:
+// const maxArr = [16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54, 76, -4, 12, -35, 4, 47];
+// let maxValue = maxArr[0];
+// let maxIndex = 0;
+//
+// maxArr.forEach((element, index) => {
+//     if (element > maxValue) {
+//         maxValue = element;
+//         maxIndex = index;
+//     }
+// });
+//
+// console.log("Task 3:\nЗнайти максимальний елемент масиву та його порядковий номер.");
+// console.log("User array:", maxArr);
+// console.log("Max value:", maxValue);
+// console.log("Index of max value:", maxIndex);
+//
+// // Task_4:
+// const negArr = [16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54, 76, -4, 12, -35, 4, 47];
+// let negativeCount = 0;
+//
+// negArr.forEach((element) => {
+//     if (element < 0) {
+//         negativeCount++;
+//     }
+// });
+//
+// console.log("Task 4:\nВизначити кількість негативних елементів.");
+// console.log("User array:", negArr);
+// console.log("Numbers of negative value:", negativeCount);
+//
+// // Task_5:
+// const oddArr = [16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54, 76, -4, 12, -35, 4, 47];
+// let oddPosCount = 0;
+//
+// oddArr.forEach((element) => {
+//     if (element > 0 && element % 2 !== 0) {
+//         oddPosCount++;
+//     }
+// });
+//
+// console.log("Task 5:\nЗнайти кількість непарних позитивних елементів.");
+// console.log("User array:", oddArr);
+// console.log("Numbers of odd positive value:", oddPosCount);
+//
+// // Task_6:
+// const pairArr = [16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54, 76, -4, 12, -35, 4, 47];
+// let pairPosCount = 0;
+//
+// pairArr.forEach((element) => {
+//     if (element > 0 && element % 2 === 0) {
+//         pairPosCount++;
+//     }
+// });
+//
+// console.log("Task 6:\nЗнайти кількість парних позитивних елементів.");
+// console.log("User array:", pairArr);
+// console.log("Numbers of pair positive value:", pairPosCount);
+//
+// // Task_7:
+// const sumPair = [16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54, 76, -4, 12, -35, 4, 47];
+// let evenPosSum = 0;
+//
+// sumPair.forEach((element) => {
+//     if (element > 0 && element % 2 === 0) {
+//         evenPosSum += element;
+//     }
+// });
+//
+// console.log("Task 7:\nЗнайти суму парних позитивних елементів.");
+// console.log("User array:", sumPair);
+// console.log("Sum of pair positive elements:", evenPosSum);
+//
+// // Task_8:
+// const sumOdd = [16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54, 76, -4, 12, -35, 4, 47];
+// let oddPosSum = 0;
+//
+// sumOdd.forEach((element) => {
+//     if (element > 0 && element % 2 !== 0) {
+//         oddPosSum += element;
+//     }
+// });
+//
+// console.log("Task 8:\nЗнайти суму непарних позитивних елементів.");
+// console.log("User array:", sumOdd);
+// console.log("Sum of odd positive elements:", oddPosSum);
+//
+// // Task_9:
+// const arrayProduct = [16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54, 76, -4, 12, -35, 4, 47];
+// let positiveProduct = 1;
+//
+// arrayProduct.forEach((element) => {
+//     if (element > 0) {
+//         positiveProduct *= element;
+//     }
+// });
+//
+// console.log("Task 9:\nЗнайти добуток позитивних елементів.");
+// console.log("User array:", arrayProduct);
+// console.log("Product of positive elements:", positiveProduct);
+//
+// // Task_10:
+// const maxElementArr = [16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54, 76, -4, 12, -35, 4, 47];
+// let maxElement = maxElementArr[0];
+// let maxElementIndex = 0;
+//
+// maxElementArr.forEach((element, index) => {
+//     if (element > maxElement) {
+//         maxElement = element;
+//         maxElementIndex = index;
+//     }
+// });
+//
+// const modifiedArray = maxElementArr.map((element, index) => (index !== maxElementIndex) ? 0 : maxElement);
+//
+// console.log("Task 10:\nЗнайти найбільший серед елементів масиву, остальні обнулити.");
+// console.log("Max value:", maxElement);
+// console.log("Modified array:", modifiedArray);
+
