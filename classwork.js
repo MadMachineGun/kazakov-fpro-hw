c38f7b64c163e6a59d0a7667ff493b697b3ad48c
Yurii Kazakov <gfnvortex@gmail.com>
2023-08-31 19:02:21 +0300

    classwork

--- classwork.js
+++ classwork.js
@@ -1,36 +1,36 @@
-
-    function howMany(selectObject) {
-    var numberSelected = 0;
-    for (var i = 0; i < selectObject.options.length; i++) {
-    if (selectObject.options[i].selected) {
-    numberSelected++;
-}
-}
-    return numberSelected;
-}
-
-    var btn = document.getElementById("btn");
-    btn.addEventListener("click", function () {
-    alert("Выбрано элементов: " + howMany(document.selectForm.musicTypes));
-});
-
-
-    var x = 0;
-    var z = 0;
-    labelCancelLoops: while (true) {
-        console.log("Внешний цикл: " + x);
-        x += 1;
-        z = 1;
-        while (true) {
-            console.log("Внутренний цикл: " + z);
-            z += 1;
-            if (z === 10 && x === 10) {
-                break labelCancelLoops;
-            } else if (z === 10) {
-                break;
-            }
-        }
-    }
+//
+//     function howMany(selectObject) {
+//     var numberSelected = 0;
+//     for (var i = 0; i < selectObject.options.length; i++) {
+//     if (selectObject.options[i].selected) {
+//     numberSelected++;
+// }
+// }
+//     return numberSelected;
+// }
+//
+//     var btn = document.getElementById("btn");
+//     btn.addEventListener("click", function () {
+//     alert("Выбрано элементов: " + howMany(document.selectForm.musicTypes));
+// });
+//
+//
+//     var x = 0;
+//     var z = 0;
+//     labelCancelLoops: while (true) {
+//         console.log("Внешний цикл: " + x);
+//         x += 1;
+//         z = 1;
+//         while (true) {
+//             console.log("Внутренний цикл: " + z);
+//             z += 1;
+//             if (z === 10 && x === 10) {
+//                 break labelCancelLoops;
+//             } else if (z === 10) {
+//                 break;
+//             }
+//         }
+//     }
 
 
 
