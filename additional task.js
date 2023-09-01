45b1a2cc65cbb17473cd4afa73c892d547f64522
Yurii Kazakov <gfnvortex@gmail.com>
2023-09-01 08:55:03 +0300

    additional_task_for_lesson_7

---
+++ additional task.js
@@ -0,0 +1,24 @@
+console.log("Additional task: Validate the phone numbers presented in the array.");
+let phoneNumbers = [
+    '+380954748389',
+    '3809748389',
+    '+380956aewrt748389',
+    '+38095632453245748389',
+    '+380456[bmb546454',
+    '',
+    '+380',
+    '+380954y48389',
+    '+380923467889',
+    '+3809234678890',
+    '+'
+];
+console.log("Source array:", phoneNumbers);
+function phoneNumberValidator(phoneNumbers) {
+    const validPhoneNumbers = phoneNumbers.filter(phoneNumber => {
+        return /^(\+380\d{9})$/.test(phoneNumber);
+    });
+
+    return validPhoneNumbers;
+}
+
+console.log("Result array:", phoneNumberValidator(phoneNumbers));
