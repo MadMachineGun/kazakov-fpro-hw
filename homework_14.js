6d3d0a746c39d1a9aa3c7b3cddd15dd7c94aba1c
Yurii Kazakov <gfnvortex@gmail.com>
2023-09-05 17:02:10 +0300

    homework_14

--- homework_14.js
+++ homework_14.js
@@ -1,5 +1,6 @@
 // Task_14.1
-console.log("Дано масив з елементами різних типів. Створити функцію яка вираховує середнє арифметичне лише числових елементів даного масиву.");
+console.log("Task 14.1: Дано масив з елементами різних типів. Створити функцію яка вираховує середнє арифметичне лише числових елементів даного масиву.");
+
 function calculateAverage(numbers) {
     const filteredNumbers = numbers.filter(item => typeof item === 'number');
 
@@ -33,3 +34,111 @@ if (!isNaN(length) && length > 0) {
     console.log("Неправильне значення довжини масиву.");
 }
 
+// Task_14.2
+console.log("Task 14.2: Написати функцію doMath(x, znak, y), яка отримує 3 аргументи: числа x і y, рядок znak. У змінній znak може бути: +, -, *, /, %, ^ (ступінь ).Вивести результат математичної дії, вказаної в змінній znak.Обидва числа і знак виходять від користувача.");
+
+function doMath(x, znak, y) {
+    x = parseFloat(x);
+    y = parseFloat(y);
+
+    if (isNaN(x) || isNaN(y)) {
+        console.log("Введіть коректні числа.");
+        return;
+    }
+
+    function calculate(x, znak, y) {
+        switch (znak) {
+            case '+':
+                return x + y;
+            case '-':
+                return x - y;
+            case '*':
+                return x * y;
+            case '/':
+                if (y !== 0) {
+                    return x / y;
+                } else {
+                    console.log("Ділення на нуль неможливе.");
+                    return null;
+                }
+            case '%':
+                if (y !== 0) {
+                    return x % y;
+                } else {
+                    console.log("Ділення за модулем на нуль неможливе.");
+                    return null;
+                }
+            case '^':
+                return Math.pow(x, y);
+            default:
+                console.log("Непідтримувана операція.");
+                return null;
+        }
+    }
+
+    const result = calculate(x, znak, y);
+
+    if (result !== null) {
+        console.log(`${x} ${znak} ${y} = ${result}`);
+    }
+}
+
+const x = prompt("Введіть перше число:");
+const znak = prompt("Введіть операцію (+, -, *, /, %, ^):");
+const y = prompt("Введіть друге число:");
+
+doMath(x, znak, y);
+
+// Task_14.3
+console.log("Task_14.3: Написати функцію заповнення даними користувача двомірного масиву. Довжину основного масиву і внутрішніх масивів задає користувач. Значення всіх елементів всіх масивів задає користувач.");
+
+function fill2DArray() {
+    const numRows = +prompt("Введіть кількість рядків:");
+    const numCols = +prompt("Введіть кількість стовпців:");
+
+    if (isNaN(numRows) || isNaN(numCols) || numRows <= 0 || numCols <= 0) {
+        console.log("Некоректні параметри. Введіть додатні цілі числа.");
+        return;
+    }
+
+    const resultArray = new Array(numRows).fill(null).map(() => new Array(numCols).fill(null));
+
+    resultArray.forEach((row, rowIndex) => row.forEach((_, colIndex) => {
+        const userInput = prompt(`Введіть значення для елементу (${rowIndex}, ${colIndex}):`);
+        row[colIndex] = userInput;
+    }));
+
+    return resultArray;
+}
+
+const twoDArray = fill2DArray();
+
+if (twoDArray) {
+    console.log("Заповнений двовимірний масив:", twoDArray);
+}
+
+
+// Task_14.4
+console.log("Task_14.4: Створити функцію, яка прибирає з рядка всі символи, які ми передали другим аргументом. 'func(\" hello world\", ['l', 'd'])' поверне нам \"heo wor\". Вихідний рядок та символи для видалення задає користувач.");
+
+const inputString = prompt("Введіть два слова через пробіл:");
+if (!inputString) {
+    console.error("Помилка: Ви не ввели слова.");
+} else {
+    const words = inputString.split(' ');
+
+    if (words.length < 2) {
+        console.error("Помилка: Має бути не менше двох слів.");
+    } else {
+        const [firstWord, secondWord] = words;
+
+        const removeCharsFromWord = (word, charsToRemove) =>
+            word.split('').filter(char => !charsToRemove.includes(char)).join('');
+        const charsToRemove = prompt("Введіть символи для видалення (через запятую або пробіл):").split(/[,\s]+/);
+        const result = [firstWord, secondWord].map(word =>
+            removeCharsFromWord(word, charsToRemove)).join(' ');
+
+        console.log("Оригінальні слова:", firstWord, secondWord);
+        console.log("Результат:", result);
+    }
+}
