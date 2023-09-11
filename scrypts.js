77691d49edd21eb25e6e95939a26f7d4b88884d2
Yurii Kazakov <gfnvortex@gmail.com>
2023-09-11 16:58:40 +0300

    homework_17

--- scrypts.js
+++ scrypts.js
@@ -1,15 +1,22 @@
+
 let currentInput = '';
+let currentNumber = '';
+let operator = '';
 let input = document.querySelector('#age-input');
 
 function addToInput(char) {
+    if (operator === '') {
+        currentNumber += char;
+    }
     currentInput += char;
     input.value = currentInput;
 }
 
 function calculate() {
     try {
-        currentInput = eval(currentInput).toFixed(2);
-        input.value = currentInput;
+        currentNumber = eval(currentInput).toFixed(2);
+        input.value = currentNumber;
+        currentInput = currentNumber;
     } catch (error) {
         currentInput = 'Error';
         input.value = currentInput;
@@ -22,25 +29,29 @@ document.querySelector('#calculate').addEventListener('click', () => {
 
 document.querySelector('#clear').addEventListener('click', () => {
     currentInput = '';
+    currentNumber = '';
+    operator = '';
     input.value = '';
 });
 
 document.querySelector('#backspace').addEventListener('click', () => {
     currentInput = currentInput.slice(0, -1);
+    currentNumber = currentNumber.slice(0, -1);
     input.value = currentInput;
 });
 
 document.querySelector('#decimal').addEventListener('click', () => {
-    if (!currentInput.includes('.')) {
+    if (!currentNumber.includes('.') || operator !== '') {
         addToInput('.');
     }
 });
 
 document.querySelector('#sqrt').addEventListener('click', () => {
-    const num = parseFloat(currentInput);
+    const num = parseFloat(currentNumber);
     if (!isNaN(num)) {
-        currentInput = Math.sqrt(num).toFixed(2);
-        input.value = currentInput;
+        currentNumber = Math.sqrt(num).toFixed(2);
+        input.value = currentNumber;
+        currentInput = currentNumber;
     } else {
         currentInput = 'Error';
         input.value = currentInput;
@@ -48,8 +59,7 @@ document.querySelector('#sqrt').addEventListener('click', () => {
 });
 
 document.querySelector('#power').addEventListener('click', () => {
-    currentInput += '**';
-    input.value = currentInput;
+    addToInput('**');
 });
 
 document.querySelector('#percent').addEventListener('click', () => {
@@ -63,17 +73,33 @@ for (let i = 0; i < 10; i++) {
 }
 
 document.querySelector('#multiply').addEventListener('click', () => {
+    if (operator !== '') {
+        calculate();
+    }
+    operator = '*';
     addToInput('*');
 });
 
 document.querySelector('#divide').addEventListener('click', () => {
+    if (operator !== '') {
+        calculate();
+    }
+    operator = '/';
     addToInput('/');
 });
 
 document.querySelector('#add').addEventListener('click', () => {
+    if (operator !== '') {
+        calculate();
+    }
+    operator = '+';
     addToInput('+');
 });
 
 document.querySelector('#subtract').addEventListener('click', () => {
+    if (operator !== '') {
+        calculate();
+    }
+    operator = '-';
     addToInput('-');
 });
