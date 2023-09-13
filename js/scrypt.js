e8caffa0d4150d97b3053936e75154008624888b
Yurii Kazakov <gfnvortex@gmail.com>
2023-09-13 15:37:20 +0300

    homework_19

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,160 +1,112 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const categories = document.querySelectorAll('.category');
-    const productList = document.querySelector('.product-list');
-    const productInfo = document.querySelector('.product-info');
-    const nextButton = document.querySelector('.next-button');
-    const loader = document.querySelector('.loader');
+    const categoriesButton = document.querySelector('.categories-button');
+    const categoriesList = document.querySelector('.categories-list ul');
+    const productsList = document.querySelector('.products-list');
 
     const productsData = [
         {
             name: 'B.C. Rich Bass',
             category: 'B.C. Rich',
-            image: 'B.C._Rich_Bass.png',
             description: 'Опис товару B.C. Rich Bass',
             price: '$899.99',
         },
         {
             name: 'B.C. Rich KKV',
             category: 'B.C. Rich',
-            image: 'B.C._Rich_KKV.png',
             description: 'Опис товару B.C. Rich KKV',
             price: '$799.99',
         },
         {
             name: 'B.C. Rich Warlock',
             category: 'B.C. Rich',
-            image: 'B.C._Rich_Warlock.png',
             description: 'Опис товару B.C. Rich Warlock',
             price: '$999.99',
         },
         {
             name: 'Gibson Explorer',
             category: 'Gibson',
-            image: 'Gibson_Explorer.png',
             description: 'Опис товару Gibson Explorer',
             price: '$1249.99',
         },
         {
             name: 'Gibson Flying V',
             category: 'Gibson',
-            image: 'Gibson_Flying_V.png',
             description: 'Опис товару Gibson Flying V',
             price: '$1149.99',
         },
         {
             name: 'Gibson SG Special',
             category: 'Gibson',
-            image: 'Gibson_SG_Special.png',
             description: 'Опис товару Gibson SG Special',
             price: '$1099.99',
         },
         {
             name: 'Jackson DK2M',
             category: 'Jackson',
-            image: 'Jackson_DK2M.png',
             description: 'Опис товару Jackson DK2M',
             price: '$799.99',
         },
         {
             name: 'Jackson Kelly',
             category: 'Jackson',
-            image: 'Jackson_Kelly.png',
             description: 'Опис товару Jackson Kelly',
             price: '$849.99',
         },
         {
             name: 'Jackson Rhoads',
             category: 'Jackson',
-            image: 'Jackson_Rhoads.png',
             description: 'Опис товару Jackson Rhoads',
             price: '$899.99',
         },
     ];
 
+    // Функция для отображения товаров по выбранной категории
     function displayProducts(category) {
-        productList.innerHTML = '';
+        productsList.innerHTML = '';
         const filteredProducts = productsData.filter(product => product.category === category);
         filteredProducts.forEach(product => {
-            const listItem = document.createElement('li');
-            listItem.classList.add('product');
-            listItem.dataset.productName = product.name;
-            listItem.innerHTML = `
-                <div class="product">
-                    <img src="img/${product.image}" alt="${product.name}">
-                    <p>${product.name}</p>
-                    <p>Ціна: ${product.price}</p>
-                    <button class="buy-button">Купити</button>
-                </div>
+            const productItem = document.createElement('div');
+            productItem.classList.add('product-item');
+            productItem.innerHTML = `
+                <h3>${product.name}</h3>
+                <p>${product.description}</p>
+                <p>Ціна: ${product.price}</p>
+                <button class="add-to-cart">Додати в корзину</button>
             `;
-            productList.appendChild(listItem);
+            productsList.appendChild(productItem);
         });
     }
 
-    function getNextProduct() {
-        const filteredProducts = productsData.filter(product => product.category === currentCategory);
-        currentProductIndex = (currentProductIndex + 1) % filteredProducts.length;
-        return filteredProducts[currentProductIndex];
-    }
+    // Обработчик клика по кнопке "Категории"
+    categoriesButton.addEventListener('click', () => {
+        categoriesList.classList.toggle('visible');
+    });
 
-    let currentCategory = 'B.C. Rich';
-    let currentProductIndex = 0;
-
-    function showLoader() {
-        loader.style.display = 'flex';
-    }
-
-    function hideLoader() {
-        loader.style.display = 'none';
-    }
-
-    showLoader();
-
-    setTimeout(function () {
-        hideLoader();
-        displayProducts(currentCategory);
-    }, 2000);
-
-    categories.forEach(category => {
-        category.addEventListener('click', () => {
-            const selectedCategory = category.getAttribute('data-category');
-            currentCategory = selectedCategory;
-            currentProductIndex = 0;
+    // Обработчик выбора категории
+    categoriesList.addEventListener('click', event => {
+        if (event.target.tagName === 'LI') {
+            const selectedCategory = event.target.getAttribute('data-category');
             displayProducts(selectedCategory);
-            productInfo.innerHTML = '';
-        });
-    });
-
-    productList.addEventListener('click', event => {
-        if (event.target.closest('.product')) {
-            const productName = event.target.closest('.product').dataset.productName;
-            const selectedProduct = productsData.find(product => product.name === productName);
-            productInfo.innerHTML = `
-                <h3>${selectedProduct.name}</h3>
-                <p>${selectedProduct.description}</p>
-                <p>Ціна: ${selectedProduct.price}</p>
-                <button class="buy-button">Купити</button>
-            `;
         }
     });
 
-    productInfo.addEventListener('click', event => {
-        if (event.target.classList.contains('buy-button')) {
-            alert('Товар куплено!');
-            productInfo.innerHTML = '';
+    // Обработчик клика по кнопке "Додати в корзину"
+    productsList.addEventListener('click', event => {
+        if (event.target.classList.contains('add-to-cart')) {
+            alert('Товар додано в корзину!');
         }
     });
 
-    nextButton.addEventListener('click', () => {
-        const nextProduct = getNextProduct();
-        if (nextProduct) {
-            const selectedProduct = productsData.find(product => product.name === nextProduct.name);
-            productInfo.innerHTML = `
-                <h3>${selectedProduct.name}</h3>
-                <p>${selectedProduct.description}</p>
-                <p>Ціна: ${selectedProduct.price}</p>
-                <button class="buy-button">Купити</button>
-            `;
-        }
+    // Обработчик отправки формы заказа
+    const checkoutForm = document.getElementById('checkout-form');
+    const purchaseSuccessMessage = document.querySelector('.purchase-success-message');
+    checkoutForm.addEventListener('submit', event => {
+        event.preventDefault();
+        // Здесь можно добавить логику отправки заказа на сервер
+        purchaseSuccessMessage.style.display = 'block';
+        setTimeout(() => {
+            purchaseSuccessMessage.style.display = 'none';
+            categoriesList.classList.add('visible'); // Возвращаем список категорий
+        }, 2000);
     });
 });
-
