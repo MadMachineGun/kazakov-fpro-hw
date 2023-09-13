485bdec31890b1bdf0545b475772de25c0f8c2a2
Yurii Kazakov <gfnvortex@gmail.com>
2023-09-13 11:01:54 +0300

    homework_18

--- scrypt.js
+++ scrypt.js
@@ -1,17 +1,44 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//     const images = [
+//         'B.C._Rich_Bass.png',
+//         'B.C._Rich_KKV.png',
+//         'B.C._Rich_Warlock.png',
+//         'Gibson_Explorer.png',
+//         'Gibson_Flying_V.png',
+//         'Gibson_SG_Special.png',
+//         'Jackson_DK2M.png',
+//         'Jackson_Kelly.png',
+//         'Jackson_Rhoads.png'
+//     ];
+//
+//     const imageContainer = document.getElementById('image-container');
+//     const nextButton = document.getElementById('next-button');
+//
+//     nextButton.addEventListener('click', showRandomImage);
+//
+//     function showRandomImage() {
+//         const randomIndex = Math.floor(Math.random() * images.length);
+//         const randomImage = images[randomIndex];
+//         imageContainer.style.backgroundImage = `url(img/${randomImage})`;
+//     }
+//
+//     showRandomImage();
+// });
 document.addEventListener('DOMContentLoaded', function () {
     const images = [
-        'B.C. Rich Bass.png',
-        'B.C. Rich KKV.png',
-        'B.C. Rich Warlock.png',
-        'Gibson Explorer.png',
-        'Gibson Flying V.png',
-        'Gibson SG Special.png',
-        'Jackson DK2M.png',
-        'Jackson Kelly.png',
-        'Jackson Rhoads.png'
+        'B.C._Rich_Bass.png',
+        'B.C._Rich_KKV.png',
+        'B.C._Rich_Warlock.png',
+        'Gibson_Explorer.png',
+        'Gibson_Flying_V.png',
+        'Gibson_SG_Special.png',
+        'Jackson_DK2M.png',
+        'Jackson_Kelly.png',
+        'Jackson_Rhoads.png'
     ];
 
     const imageContainer = document.getElementById('image-container');
+    const imageName = document.getElementById('image-name');
     const nextButton = document.getElementById('next-button');
 
     nextButton.addEventListener('click', showRandomImage);
@@ -20,6 +47,8 @@ document.addEventListener('DOMContentLoaded', function () {
         const randomIndex = Math.floor(Math.random() * images.length);
         const randomImage = images[randomIndex];
         imageContainer.style.backgroundImage = `url(img/${randomImage})`;
+        imageName.textContent = randomImage.replace(/_/g, ' ').replace(/\.\w+$/, '');
+
     }
 
     showRandomImage();
