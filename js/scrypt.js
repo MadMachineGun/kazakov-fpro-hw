d850b47dbcaab6dd24b991b04db7ae7df4334a12
Yurii Kazakov <gfnvortex@gmail.com>
2023-09-15 14:21:41 +0300

    homework_20

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,4 +1,38 @@
-
+//
+// const imageCount = 41;
+// const imagePrefix = "img/";
+// const images = [];
+//
+// for (let i = 1; i <= imageCount; i++) {
+//     images.push(`${imagePrefix}${i}.jpg`);
+// }
+//
+// let currentIndex = 0;
+// const prevButton = document.getElementById("prev");
+// const nextButton = document.getElementById("next");
+// const sliderImage = document.getElementById("slider-image");
+//
+// function updateImage() {
+//     sliderImage.src = images[currentIndex];
+//     prevButton.style.display = currentIndex === 0 ? "none" : "block";
+//     nextButton.style.display = currentIndex === images.length - 1 ? "none" : "block";
+// }
+//
+// prevButton.addEventListener("click", () => {
+//     if (currentIndex > 0) {
+//         currentIndex--;
+//         updateImage();
+//     }
+// });
+//
+// nextButton.addEventListener("click", () => {
+//     if (currentIndex < images.length - 1) {
+//         currentIndex++;
+//         updateImage();
+//     }
+// });
+//
+// updateImage();
 const imageCount = 41;
 const imagePrefix = "img/";
 const images = [];
@@ -11,6 +45,20 @@ let currentIndex = 0;
 const prevButton = document.getElementById("prev");
 const nextButton = document.getElementById("next");
 const sliderImage = document.getElementById("slider-image");
+const audioElement = document.getElementById("myAudio");
+
+audioElement.src = "back_in_black.mp3";
+
+let isNextButtonClicked = false;
+
+function playAudioOnNextClick() {
+    if (!isNextButtonClicked) {
+        audioElement.play();
+        isNextButtonClicked = true;
+    }
+}
+
+nextButton.addEventListener("click", playAudioOnNextClick);
 
 function updateImage() {
     sliderImage.src = images[currentIndex];
