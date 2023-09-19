1c59d6e9b384ba7f7a29f245e80535db0b0b4d75
Yurii Kazakov <gfnvortex@gmail.com>
2023-09-19 10:03:18 +0300

    homework_21

--- js/script.js
+++ js/script.js
@@ -1,34 +1,60 @@
-// Функція для відображення даних у таблиці
+
 function displayUserData(userData) {
     const table = document.querySelector('table');
-    const newRow = table.insertRow();
+
     for (const key in userData) {
-        const cell = newRow.insertCell();
-        cell.textContent = userData[key];
+        const newRow = table.insertRow();
+        const cell1 = newRow.insertCell();
+        cell1.textContent = key;
+        const cell2 = newRow.insertCell();
+        cell2.textContent = userData[key];
     }
 }
 
-// Обробник події для кнопки "Зберегти"
 document.getElementById('save-button').addEventListener('click', function() {
-    const formData = new FormData(document.getElementById('registration-form'));
-    const userData = {};
-    formData.forEach((value, key) => {
-        userData[key] = value;
+    const firstName = document.getElementById('first-name').value;
+    const lastName = document.getElementById('last-name').value;
+    const email = document.getElementById('email').value;
+    const birthdate = document.getElementById('birthdate').value;
+    const gender = document.querySelector('input[name="gender"]:checked').value;
+    const city = document.getElementById('city').value;
+
+    const languages = [];
+    const languageCheckboxes = document.querySelectorAll('input[name="languages"]:checked');
+    languageCheckboxes.forEach(checkbox => {
+        languages.push(checkbox.value);
     });
 
-    // Відображення даних у таблиці
+    const dateRegex = /^\d{2}\.\d{2}\.\d{4}$/;
+    if (!dateRegex.test(birthdate)) {
+        alert('Please enter a valid date of birth in the format DD.MM.YYYY');
+        return;
+    }
+
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!emailRegex.test(email)) {
+        alert('Please enter a valid email address');
+        return;
+    }
+
+    const userData = {
+        'First Name': firstName,
+        'Last Name': lastName,
+        'Email': email,
+        'Date of Birth': birthdate,
+        'Gender': gender,
+        'City': city,
+        'Languages': languages.join(', '),
+    };
+
     displayUserData(userData);
 
-    // Сховати форму та показати таблицю
     document.getElementById('registration-form').style.display = 'none';
     document.getElementById('user-table').style.display = 'block';
 });
 
-// Обробник події для кнопки "Subscribe"
 document.getElementById('subscribe-button').addEventListener('click', function() {
-    // Сховати таблицю та показати форму
     document.getElementById('user-table').style.display = 'none';
     document.getElementById('registration-form').style.display = 'block';
-    // Очистити дані форми
     document.getElementById('registration-form').reset();
 });
