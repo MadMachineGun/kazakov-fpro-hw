c005d44ab985b965e13b0fbd23cddb49cb731d68
Yurii Kazakov <gfnvortex@gmail.com>
2023-09-20 12:00:07 +0300

    homework_22

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 
-const число1 = parseFloat(prompt('Введіть перше число:'));
-const число2 = parseFloat(prompt('Введіть друге число:'));
-const число3 = parseFloat(prompt('Введіть третє число:'));
+const num1 = parseFloat(prompt('Введіть перше число:'));
+const num2 = parseFloat(prompt('Введіть друге число:'));
+const num3 = parseFloat(prompt('Введіть третє число:'));
 
 
-if (isNaN(число1) || isNaN(число2) || isNaN(число3)) {
+if (isNaN(num1) || isNaN(num2) || isNaN(num3)) {
     alert('Ви ввели некоректне значення. Будь ласка, введіть числа.');
 } else {
 
-    const середнє = (число1 + число2 + число3) / 3;
+    const average = (num1 + num2 + num3) / 3;
 
 
-    alert(`Середнє арифметичне введених чисел: ${середнє}`);
+    alert(`Середнє арифметичне введених чисел: ${average}`);
 }
