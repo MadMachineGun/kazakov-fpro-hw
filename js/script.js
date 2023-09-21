41b87b079f2183c95825d836ee25d68dae6a74a2
Yurii Kazakov <gfnvortex@gmail.com>
2023-09-21 11:30:15 +0300

    homework_21+audio

--- js/script.js
+++ js/script.js
@@ -1,1 +1,64 @@
 `use strict`
+//Create smileys data
+const smileys = [
+    {
+        smile: "😆",
+        voteCount: 0,
+    },
+    {
+        smile: "😅",
+        voteCount: 0,
+    },
+    {
+        smile: "😃",
+        voteCount: 0,
+    },
+    {
+        smile: "😈",
+        voteCount: 0,
+    },
+    {
+        smile: "😉",
+        voteCount: 0,
+    },
+    {
+        smile: "😉",
+        voteCount: 0,
+    },
+];
+
+//Get smile && vote containers
+let smileContainer = document.querySelector('#smile-container');
+let voteContainer = document.querySelector('#vote-container');
+
+//Update vote scores
+function updateVotes() {
+    voteContainer.innerHTML = "";
+
+    smileys.forEach((item) => {
+        let voteElement = document.createElement('div');
+        voteElement.classList.add('vote-score');
+        voteElement.textContent = item.voteCount;
+
+        voteContainer.appendChild(voteElement);
+    })
+}
+
+//Show smileys elements
+function showSmiles() {
+    smileys.forEach((item, index) => {
+        let smileElement = document.createElement('div');
+        smileElement.textContent = item.smile;
+
+        smileElement.addEventListener('click', () => {
+            smileys[index].voteCount++;
+            updateVotes();
+        })
+
+        smileContainer.appendChild(smileElement);
+    })
+}
+
+//Initialization
+showSmiles();
+updateVotes();
