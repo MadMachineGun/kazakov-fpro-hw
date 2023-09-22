e46b82d296124a99c773ff2ad41d52ba3a292bb2
Yurii Kazakov <gfnvortex@gmail.com>
2023-09-22 14:28:22 +0300

    homework_24

---
+++ script.js
@@ -0,0 +1,10 @@
+`use strict`
+console.log(`Task 22.1: Отримати від користувача 3 рядки та вивести їх у довільному порядку однією командою (конкатенація).`);
+
+const input1 = prompt('Введіть перший рядок:');
+const input2 = prompt('Введіть другий рядок:');
+const input3 = prompt('Введіть третій рядок:');
+
+const result = input1 + input2 + input3;
+console.log('Результат конкатенації:', result);
+
