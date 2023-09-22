56628c55a8804475495f6e7b5a87ea9ea51a6a51
Yurii Kazakov <gfnvortex@gmail.com>
2023-09-22 14:44:37 +0300

    homework_24

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
 `use strict`
-console.log(`Task 22.1: Отримати від користувача 3 рядки та вивести їх у довільному порядку однією командою (конкатенація).`);
+console.log(`Task 24.1: Отримати від користувача 3 рядки та вивести їх у довільному порядку однією командою (конкатенація).`);
 
-const input1 = prompt('Введіть перший рядок:');
-const input2 = prompt('Введіть другий рядок:');
-const input3 = prompt('Введіть третій рядок:');
+const input1 = prompt("Введіть перший рядок:");
+const input2 = prompt("Введіть другий рядок:");
+const input3 = prompt("Введіть третій рядок:");
 
-const result = input1 + input2 + input3;
-console.log('Результат конкатенації:', result);
+if (input1 !== null && input2 !== null && input3 !== null) {
+    const inputs = [input1, input2, input3];
+    const shuffledInputs = shuffleArray(inputs);
+    const result = shuffledInputs.join('');
+    console.log("Результат конкатенації:", result);
+} else {
+    console.log("Скасовано або не введено всі значення.");
+}
+
+function shuffleArray(array) {
+    const shuffled = [...array];
+    for (let i = shuffled.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+    }
+    return shuffled;
+}
+
+
+console.log(`Task 24.2: Розбити за цифрами п'ятизначне число і вивести у вихідному порядку через пробіл.`);
+
+const number = prompt('Введіть п\'ятизначне число:');
+if (number.length !== 5 || isNaN(number)) {
+    console.log('Введене значення не є п\'ятизначним числом.');
+} else {
+
+    const digits = number.split('');
+    console.log('Розділені цифри:', digits.join(' '));
+}
+
 
