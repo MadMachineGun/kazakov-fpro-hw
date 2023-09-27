7fdfdc90324c664a769de828f03cf253c836cbc8
Yurii Kazakov <gfnvortex@gmail.com>
2023-09-27 09:34:56 +0300

    homework_28_empty

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-`use strict`
+"use strict";
+
+const tableContainer = document.querySelector(".table-container");
+
+const table = document.createElement("table");
+tableContainer.appendChild(table);
+
+let cellValue = 1;
+for (let i = 1; i <= 10; i++) {
+    const row = document.createElement("tr");
+    for (let j = 1; j <= 10; j++) {
+        const cell = document.createElement("td");
+
+        cell.textContent = cellValue.toString();
+        row.appendChild(cell);
+
+        cellValue++;
+    }
+
+    table.appendChild(row);
+}
