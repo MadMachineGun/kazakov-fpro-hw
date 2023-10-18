1161ecc635b2834bfdfd820b7b4eec0fe8da0429
Yurii Kazakov <gfnvortex@gmail.com>
2023-10-18 20:35:35 +0300

    classwork_37

--- classwork.js
+++ classwork.js
@@ -1,2 +1,15 @@
 `use strict`
 
+// function add(a, b, c, d){
+//     return a + b + c + d;
+// }
+// const arr= [1, 2, 3, 4];
+// const arrCopy = [...arr];
+//
+// console.log(add(...arr));
+// console.log(arrCopy);
+
+
+
+
+
