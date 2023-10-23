79618fbc67834e7c2ecbb7c7760f4c538890deb3
Yurii Kazakov <gfnvortex@gmail.com>
2023-10-23 20:24:16 +0300

    server

--- app.js
+++ app.js
@@ -1,40 +1,43 @@
-// const http = require(`http`);
-// const PORT = 3500;
-// const user =
-//     {
-//         name: `Yurii`,
-//         id: 3
-//     }
-// const contact = {
-//     telegram: `Vortex`
-// }
-//
-//
-// http.createServer(function (request, response) {
-//     const url = request.url;
-//     response.setHeader("content-Type", "application/json; charset=utf-8;");
-//     switch (url) {
-//         case `/`:
-//             response.write(JSON.stringify(user));
-//             break;
-//         case `/contact`:
-//             response.write(JSON.stringify(contact));
-//             break;
-//         default:
-//             response.setHeader("content-Type", "text/html; charset=utf-8;");
-//             response.write("<h1>404</h1>");
-//     }
-//
-//     console.log(request);
-//     console.log(request.method);
-//
-//     response.end();
-// }).listen(PORT);
+const http = require(`http`);
+const PORT = 3500;
+const user =
+    {
+        name: `Yurii`,
+        id: 3
+    }
+const contact = {
+    telegram: `Vortex`
+}
 
-const getUserBtn = document.querySelector(`#get-user`);
-const getContactBtn = document.querySelector(`#get-contact`);
 
-const baseUrl = `http://`
+http.createServer(function (request, response) {
+    const url = request.url;
+    response.setHeader("content-Type", "application/json; charset=utf-8;");
+    response.setHeader(`Access-Control-Allow-Origin`, `*`);
+    response.setHeader(`Access-Control-Allow-Methods`, `GET, POST`);
+    response.setHeader();
+    switch (url) {
+        case `/`:
+            response.write(JSON.stringify(user));
+            break;
+        case `/contact`:
+            response.write(JSON.stringify(contact));
+            break;
+        default:
+            response.setHeader("content-Type", "text/html; charset=utf-8;");
+            response.write("<h1>404</h1>");
+    }
+
+    console.log(request);
+    console.log(request.method);
+
+    response.end();
+}).listen(PORT);
+
+// const getUserBtn = document.querySelector(`#get-user`);
+// const getContactBtn = document.querySelector(`#get-contact`);
+//
+// const baseUrl = `http://`
 
 
 
