a46e22350dbb5cd55dbf49a7529e96902fdc0f8c
Yurii Kazakov <gfnvortex@gmail.com>
2023-10-23 16:01:15 +0300

    homework_38

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -2,5 +2,8 @@ const {src, dest} = require(`gulp`);
 const sass = require('gulp-sass')(require('sass'));
 
 function convertScss() {
-return
+    return src(`app/scss/style.scss`)
+        .pipe(scss())
+        .pipe(dest(app/css));
+
 }
