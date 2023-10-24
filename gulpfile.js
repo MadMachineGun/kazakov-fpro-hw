83256b7c93519f9d69c13164482656a67877e210
Yurii Kazakov <gfnvortex@gmail.com>
2023-10-24 09:38:37 +0300

    homework_38

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,22 @@
 const {src, dest} = require(`gulp`);
 const scss = require('gulp-sass')(require('sass'));
+const concat = require(`gulp-concat`);
+const uglify = require(`gulp-uglify-es`).default;
 
 function convertScss() {
     return src(`app/scss/styles.scss`)
-        .pipe(scss())
+        .pipe(concat(`style.min.css`))
+        .pipe(scss({outputStyle: `compressed`}))
         .pipe(dest(`app/css`));
 
 }
+
+function convertJS() {
+    return src(`app/js/main.js`)
+        .pipe(concat(`main.min.js`))
+        .pipe(uglify())
+        .pipe(dest(`app/js`));
+}
+
 exports.convertScss = convertScss;
+exports.convertJS = convertJS;
