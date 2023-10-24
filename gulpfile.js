a62c1273b1e9af6e864f4c2b9eff5852d004089f
Yurii Kazakov <gfnvortex@gmail.com>
2023-10-24 15:08:38 +0300

    homework_38

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,14 @@ const {src, dest, watch} = require(`gulp`);
 const scss = require('gulp-sass')(require('sass'));
 const concat = require(`gulp-concat`);
 const uglify = require(`gulp-uglify-es`).default;
+const browserSync = require('browser-sync').create();
 
 function convertScss() {
     return src(`app/scss/styles.scss`)
         .pipe(concat(`style.min.css`))
         .pipe(scss({outputStyle: `compressed`}))
-        .pipe(dest(`app/css`));
+        .pipe(dest(`app/css`))
+        .pipe(browserSync.stream());
 
 }
 
@@ -15,7 +17,8 @@ function convertJS() {
     return src(`app/js/main.js`)
         .pipe(concat(`main.min.js`))
         .pipe(uglify())
-        .pipe(dest(`app/js`));
+        .pipe(dest(`app/js`))
+        .pipe(browserSync.stream());
 }
 
 function watching() {
@@ -23,6 +26,15 @@ function watching() {
     watch([`app/scss/styles.scss`], convertScss);
 }
 
+function browserAutoUpdate() {
+    browserSync.init({
+        server: {
+            baseDir: "app"
+        }
+    });
+}
+
 exports.convertScss = convertScss;
 exports.convertJS = convertJS;
 exports.watching = watching;
+exports.browserAutoUpdate = browserAutoUpdate;
